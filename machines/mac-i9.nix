19a9d15f5dda0005f8a5c11617c564ddbc171058
Anton Shapka <anton.shapka@oracle.com>
2025-08-04 12:37:59 +0300

    New packages

--- machines/mac-i9.nix
+++ machines/mac-i9.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }: {
+  imports = [
+    ../pkgs/dev.nix
+  ];
   environment.systemPackages = with pkgs; [
     cachix
   ];
--- pkgs/dev.nix
+++ pkgs/dev.nix
@@ -3,5 +3,6 @@
   environment.systemPackages = with pkgs; [
 #    gcc
     cmake
+    sonar-scanner-cli
   ];
 }
