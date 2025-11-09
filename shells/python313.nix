cb13b2ae6f0fee5214a43a7ad548bff6da0779a9
Anton Shapka <anton.shapka@oracle.com>
2025-11-10 01:23:21 +0200

    Fix python313 shell

--- shells/python313.nix
+++ shells/python313.nix
@@ -2,16 +2,9 @@
 
 pkgs.mkShell {
   packages = with pkgs; [
+  	poetry
   	python313
-  	python313Packages.tkinter
-  	python313Packages.pylzma
   	python313Packages.pip
   	python313Packages.poetry-core
-  	python313Packages.huggingface-hub
-  	python313Packages.hf-xet
-  	python313Packages.conda
-  	python313Packages.libmambapy
-  	python313Packages.xonsh
-  	python313Packages.pre-commit-hooks
   ];
 }
