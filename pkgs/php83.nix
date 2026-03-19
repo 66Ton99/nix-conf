407c8047a853e2a53659d66dffbdc487bb425071
Anton Shapka <anton.shapka@oracle.com>
2026-03-19 17:16:43 +0200

    Fix slow PHP

--- pkgs/php83.nix
+++ pkgs/php83.nix
@@ -1,17 +1,37 @@
 # PHP 8.3
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  php83Fast = pkgs.php83.buildEnv {
+    extensions = ({ enabled, all }: enabled ++ (with all; [
+      xdebug
+      apcu
+      xsl
+    ]));
+    extraConfig = ''
+      xdebug.mode=debug
+      apc.enable_cli=1
+      memory_limit=-1
+    '';
+  };
+
+  php83Oci = pkgs.php83.buildEnv {
+    extensions = ({ enabled, all }: enabled ++ (with all; [
+      xdebug
+      apcu
+      oci8
+      xsl
+    ]));
+    extraConfig = ''
+      xdebug.mode=debug
+      apc.enable_cli=1
+      memory_limit=-1
+    '';
+  };
+in {
   environment.systemPackages = [
-    (pkgs.php83.buildEnv {
-      extensions = ({ enabled, all }: enabled ++ (with all; [
-        xdebug
-        apcu
-        oci8
-        xsl
-      ]));
-      extraConfig = ''
-        xdebug.mode=debug
-        apc.enable_cli=1
-      '';
-    })
+    php83Fast
+    (pkgs.writeShellScriptBin "php-oci" ''
+      exec ${php83Oci}/bin/php "$@"
+    '')
   ];
 }
